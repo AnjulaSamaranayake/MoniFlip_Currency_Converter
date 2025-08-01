e117adde1f3b0d6fadf70f8af0999c2d0893ef54
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2025-08-01 21:27:24 +0530

    store client data

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function MainPage() {
+
+    // state for the date, source currency, target currency, and amount
+    const [date, setDate] = useState(null);
+    const [sourceCurrency, setSourceCurrency] = useState('');
+    const [targetCurrency, setTargetCurrency] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [conversionResult, setConversionResult] = useState(0);
+
+    //handle dubmit method
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(date, setSourceCurrency, targetCurrency, amount);
+    }
+
   return (
     <div>
         <h1 className='lg:mx-32 text-5xl font-bold text-blue-500'>MoniFlip</h1>
@@ -8,34 +22,34 @@ export default function MainPage() {
 
         <div className='mt-5 flex items-center justify-center flex-col'>
             <section className='w-full lg:w-1/2'>
-                <form>
+                <form onSubmit={handleSubmit}>
                         <div className='mb-6'>
                             <div>
-                                <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-                                <input type="Date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                <label htmlFor={date} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                                <input onChange={(e) => setDate(e.target.value)} type="Date" id="date" name='date' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
 
                             <div>
-                                <label for="sourceCurrency" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
-                                <select className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='' id='' >
+                                <label htmlFor={sourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
+                                <select onChange={(e) => setSourceCurrency(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='sourceCurrency' id='sourceCurrency' >
                                     <option value="">Select your source currency</option>
                                 </select>
                             </div>
 
                             <div>
-                                <label for="targetCurrency" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
-                                <select className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='' id='' >
+                                <label htmlFor={targetCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
+                                <select onChange={(e) => setTargetCurrency(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='targetCurrency' id='targetCurrency' >
                                     <option value="">Select your target currency</option>
                                 </select>
                             </div>
 
                              <div>
-                                <label for="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount in Source Currency</label>
-                                <input type="text" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Amount in source currency .....' required />
+                                <label htmlFor={amount} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount in Source Currency</label>
+                                <input onChange={(e) => setAmount(e.target.value)} type="number" id="amount" name='amount' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Amount in source currency .....' required />
                             </div>
                         </div>
-                        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
                             Get the Conversion
                         </span>
                         </button>
